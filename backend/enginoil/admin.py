a1cf90129d6a27903a43d7a66e78bfd701345ebc
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-11-17 18:27:30 +0900

    [backend] enginoil model 변경

--- backend/enginoil/admin.py
+++ backend/enginoil/admin.py
@@ -5,8 +5,8 @@ from .models import EnginOil
 @admin.register(EnginOil)
 class EnginOilAdmin(admin.ModelAdmin):
     list_display = [
-        'id', 'center', 'change_km', 'change_date', 'enginoil_img', 'author',
+        'id', 'center', 'change_km', 'change_date', 'enginoil_img', 'userid',
     ]
-    list_filter = ['center', 'author']
-    search_fields = ['center', 'change_date', 'author']
+    list_filter = ['center', 'userid']
+    search_fields = ['center', 'change_date', 'userid']
     list_per_page = 30
--- backend/enginoil/models.py
+++ backend/enginoil/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class EnginOil(models.Model):
-    author = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    userid = models.CharField(max_length=255, null=False, blank=False)
     change_date = models.DateField(null=False)
     enginoil_img = models.ImageField(blank=True, upload_to='enginoil/img')
     center = models.CharField(max_length=100, null=False)
     change_km = models.IntegerField(null=False)
 
     def __str__(self):
-        return str(self.author)+"_"+str(self.change_date)
+        return "enginoil"+str(self.userid)+"_"+str(self.change_date)
