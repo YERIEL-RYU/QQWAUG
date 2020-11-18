bcf8a1fb2b7ed2b6efbbaa56a3eb9354ddbb56b6
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-11-18 18:13:54 +0900

    [backend] enginoil view 생성

--- backend/enginoil/admin.py
+++ backend/enginoil/admin.py
@@ -5,7 +5,7 @@ from .models import EnginOil
 @admin.register(EnginOil)
 class EnginOilAdmin(admin.ModelAdmin):
     list_display = [
-        'id', 'center', 'change_km', 'change_date', 'enginoil_img', 'userid',
+        'id', 'center', 'change_km', 'change_date', 'enginoil_img', 'userid', 'image_tag'
     ]
     list_filter = ['center', 'userid']
     search_fields = ['center', 'change_date', 'userid']
--- backend/enginoil/views.py
+++ backend/enginoil/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+from .models import EnginOil
+from .serializers import EnginOilSerializer
+
+
+class enginoilList(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = EnginOilSerializer
+    parser_classes = (JSONParser, MultiPartParser)
+
+    def get(self, request, format=None):
+        queryset = EnginOil.objects.all()
+        serializer = EnginOilSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = EnginOilSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class enginoilDetail(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = EnginOilSerializer
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+    def get_object(self, userid):
+        try:
+            return EnginOil.objects.get(userid=userid)
+        except:
+            raise Http404
+
+    def get(self, request, userid):
+        queryset = self.get_object(userid)
+        serializer = EnginOilSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
